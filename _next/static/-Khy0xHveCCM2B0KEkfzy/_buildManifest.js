0b894416ab1051234ac0358a16ec842279129db9
git-katalist <79943903+git-katalist@users.noreply.github.com>
2022-12-05 14:21:47 +0000

    Deploying to gh-pages from @ git-katalist/clumsy@01f62904aa64bcd8d42af18727156b3e81cf1f3a 🚀

--- _next/static/-Khy0xHveCCM2B0KEkfzy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ffe41784fe78e8a7.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/CAXf1SHnEPCP8WA_Ug6PG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9b06a32f27c4997c.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
